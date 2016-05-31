1aa7f4ebd9e5435220b70a5e91efa9b2824e1277
johaen11 <johngnanakumar@gmail.com>
2016-05-31 12:50:34 -0400

    Add project files.

---
+++ COMP123-Assignment2/Hero.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Assignment2
+{
+    public class Hero
+    {
+        //Private Instance Variables
+        private int _strength;
+        private int _speed;
+        private int _health;
+        private string _name;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+
+        }
+
+        private void _generateAbilities()
+        {
+            Random rnd = new Random();
+            _strength = rnd.Next(1, 101);
+            _speed = rnd.Next(1, 101);
+            _health = rnd.Next(1, 101);
+        }
+
+        private bool _hitAttempt()
+        {
+            Random rnd = new Random();
+            if (rnd.Next(1, 101) <= 20)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private int _hitDamage()
+        {
+            Random rnd = new Random();
+
+            return rnd.Next(1, 7) * _strength;
+
+
+        }
+
+        public void Fight()
+        {
+            if (_hitAttempt() == true)
+            {
+                _hitDamage();
+                Console.WriteLine("The Damage hit was {0}", _hitDamage());
+            }
+            else
+                Console.WriteLine("Hit Attempt Failed");
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Strength: {0}", _strength);
+            Console.WriteLine("Health: {0}", _health);
+            Console.WriteLine("Speed: {0}", _speed);
+
+
+        }
+        public Hero(string name)
+        {
+            _name = name;
+            _generateAbilities();
+        }
+    }
+}
---
+++ COMP123-Assignment2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**
+ * Author's Name: Johaen Gnanakumar
+ * Author's Student#:300880309
+ * Date Last Modified:05/28/2016
+ * Program Description:
+ * Revision History: Version 0.06
+ */
+namespace COMP123_Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero hero = new Hero("Johaen");
+            hero.Show();
+            hero.Fight();
+        }
+    }
+}
